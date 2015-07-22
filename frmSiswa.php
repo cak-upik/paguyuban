882023ae2f678ba28833128dd851394f6845433f
cak-upik <muhammadtaufik92@gmail.com>
2015-07-22 14:00:12 +0700

    Update Create & Read frmSiswa.php, add function to save, and load data in siswaDao.php

--- frmSiswa.php
+++ frmSiswa.php
@@ -102,49 +102,23 @@ if (isset($_GET['page'])) {
                                     <h3>Data Siswa</h3>
                                     <div class="form-actions">
                                     <a href="index.php?pgy=siswa&page=create" class="btn btn-primary btn">Tambah Siswa</a>
-                                    <a href="index.php?pgy=siswa&page=create" class="btn btn-info btn">Edit Siswa</a>
+                                    <a href="index.php?pgy=siswa&page=edit" class="btn btn-info btn">Edit Siswa</a>
                                     <a href="index.php?pgy=siswa&page=create" class="btn btn-danger btn">Hapus Siswa</a>
                                     </div>
                                     <table class="table table-bordered table-striped table-highlight">
                                         <thead>
                                             <tr>
-                                                <th>No</th>
-                                                <th>First Name</th>
-                                                <th>Last Name</th>
-                                                <th>Username</th>
+                                                <th class="span1">No</th>
+                                                <th>Nama Siswa</th>
+                                                <th>Kelas</th>
+                                                <th>Alamat</th>
+                                                <th>Email</th>
+                                                <th>No.Telp</th>
+                                                <th>Layanan</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Michael</td>
-                                                <td>Jordan</td>
-                                                <td>@mjordan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>Magic</td>
-                                                <td>Johnson</td>
-                                                <td>@mjohnson</td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>Larry</td>
-                                                <td>the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
-                                            <tr>
-                                                <td>4</td>
-                                                <td>Charles</td>
-                                                <td>Barkley</td>
-                                                <td>@cbark</td>
-                                            </tr>
-                                            <tr>
-                                                <td>5</td>
-                                                <td>Karl</td>
-                                                <td>Malone</td>
-                                                <td>@kmalone</td>
-                                            </tr>
+                                        <?php LoadSiswa();?>
                                         </tbody>
                                     </table>
                                     <br />
@@ -155,6 +129,136 @@ if (isset($_GET['page'])) {
                 </div>
             </div>
         </div>
+        <?php
+    } else if ($_GET['page'] == 'edit') {
+        ?>
+        <div class="main">
+            <div class="container">
+                <div class="row">
+                    <div class="span12">      		
+                        <div class="widget stacked ">
+                            <div class="widget-header">
+                                <i class="icon-pencil"></i>
+                                <h3>Form Data Siswa</h3>
+                            </div> <!-- /widget-header -->
+
+                            <div class="widget-content">
+                                <section id="tables">
+                                    <h3>Data Siswa</h3>
+                                    <div class="form-actions">
+                                    <a href="index.php?pgy=siswa&page=create" class="btn btn-primary btn">Tambah Siswa</a>
+                                    <a href="index.php?pgy=siswa&page=edit" class="btn btn-info btn">Edit Siswa</a>
+                                    <a href="index.php?pgy=siswa&page=create" class="btn btn-danger btn">Hapus Siswa</a>
+                                    </div>
+                                    <table class="table table-bordered table-striped table-highlight">
+                                        <thead>
+                                            <tr>
+                                                <th class="span1">#</th>
+                                                <th>Nama Siswa</th>
+                                                <th>Kelas</th>
+                                                <th>Alamat</th>
+                                                <th>Email</th>
+                                                <th>No.Telp</th>
+                                                <th>Layanan</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php EditSiswa();?>
+                                        </tbody>
+                                    </table>
+                                    <br />
+                                </section>
+                            </div> <!-- /widget-content -->
+                        </div> <!-- /widget -->
+                    </div> <!-- /span12 -->
+                </div>
+            </div>
+        </div>
+        <?php
+    } else if($_GET['page'] == 'editor') {
+        ?>
+        <div class="main">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="span12">
+
+                        <div class="widget stacked">
+
+                            <div class="widget-header">
+                                <i class="icon-check"></i>
+                                <h3>Input Data Siswa</h3>
+                            </div> <!-- /widget-header -->
+
+                            <div class="widget-content">
+
+                                <br />
+
+                                <form action="index.php?pgy=siswa&do=save" method="post" class="form-horizontal" />
+                                <fieldset>
+                                    <div class="control-group">
+                                        <label class="control-label" for="name">Nama Siswa</label>
+                                        <div class="controls">
+                                            <input type="text" class="input-xxlarge" name="nama" id="nama" />
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <label class="control-label" for="kelas">Kelas</label>
+                                        <div class="controls">
+                                            <input type="text" class="input-large" name="kelas" id="kelas" />
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <label class="control-label" for="alamat">Alamat</label>
+                                        <div class="controls">
+                                            <textarea class="span4" name="alamat" id="alamat" rows="4"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <label class="control-label" for="email">Email</label>
+                                        <div class="controls">
+                                            <input type="email" class="input-large" name="email" id="email" />
+                                        </div>
+                                    </div>
+                                    <div class="control-group">
+                                        <label class="control-label" for="telp">No. Telp</label>
+                                        <div class="controls">
+                                            <input type="text" class="input-large" name="telp" id="telp" />
+                                        </div>
+                                    </div>
+
+                                    <div class="control-group">
+                                        <label class="control-label" for="status">Jenis Layanan</label>
+                                        <div class="controls">
+                                            <select id="status" name="status">
+                                                <option value="" />Select...
+                                                <option value="oneway" />One-Way
+                                                <option value="twoway" />Two-Way
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-actions">
+                                        <button type="submit" class="btn btn-danger btn">Simpan Data</button>&nbsp;&nbsp;
+                                        <a href="index.php?pgy=siswa&page=view" class="btn">Batal</a>
+                                    </div>
+                                </fieldset>
+                                </form>
+
+                            </div> <!-- /widget-content -->
+
+                        </div> <!-- /widget -->					
+
+                    </div> <!-- /span12 -->     	
+
+
+                </div> <!-- /row -->
+
+            </div> <!-- /container -->
+
+        </div> <!-- /main -->
         <?php
     }
 }
--- siswaDao.php
+++ siswaDao.php
@@ -1,18 +1,18 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 function SaveSiswa($nama, $alamat, $kelas, $email, $notelp, $layanan) {
-    $qry = "INSERT INTO siswa(nama_siswa, alamat, kelas, email, no_tlp, status) VALUES('".$nama."','".$alamat."','".$kelas."','".$email."','".$notelp."','".$layanan."')";
+    $qry = "INSERT INTO siswa(nama_siswa, alamat, kelas, email, no_tlp, status) VALUES('" . $nama . "','" . $alamat . "','" . $kelas . "','" . $email . "','" . $notelp . "','" . $layanan . "')";
     $exec = mysql_query($qry);
-    if($exec) {
+    if ($exec) {
         echo "<script>javascript:alert('Penyimpanan Data Siswa Berhasil')</script>";
         echo "<script>javascript:window.location.assign('index.php?pgy=siswa&page=view')</script>";
-    }else {
+    } else {
         echo "<script>javascript:alert('Penyimpanan Data Siswa Gagal')</script>";
         echo "<script>javascript:history.go(-1)</script>";
     }
@@ -20,6 +20,48 @@ function SaveSiswa($nama, $alamat, $kelas, $email, $notelp, $layanan) {
 }
 
 function LoadSiswa() {
-    
-    
+    $i = 1;
+    $qry = "SELECT * FROM siswa";
+    $exec = mysql_query($qry);
+    if ($exec) {
+        while ($data = mysql_fetch_array($exec)) {
+            echo "<tr>
+                      <td>".$i."</td>
+                      <td>".$data['nama_siswa']."</td>
+                      <td>".$data['alamat']."</td>
+                      <td>".$data['kelas']."</td>
+                      <td>".$data['email']."</td>
+                      <td>".$data['no_tlp']."</td>
+                      <td>".$data['status']."</td>
+                 </tr>";
+            $i++;
+        }
+    }
+}
+
+function EditSiswa() {
+    $qry = "SELECT * FROM siswa";
+    $exec = mysql_query($qry);
+    if ($exec) {
+        while ($data = mysql_fetch_array($exec)) {
+            echo "<tr>
+                      <td><input type=button class='btn btn-danger btn' name=btnId".$data['id_siswa']." value=Ubah onclick=window.location.assign('index.php?pgy=siswa&page=editor&id=$data[id_siswa]')></td>
+                      <td>".$data['nama_siswa']."</td>
+                      <td>".$data['alamat']."</td>
+                      <td>".$data['kelas']."</td>
+                      <td>".$data['email']."</td>
+                      <td>".$data['no_tlp']."</td>
+                      <td>".$data['status']."</td>
+                 </tr>";
+            $i++;
+        }
+    }
+}
+
+function getValue($field, $id, $item) {
+    $qry = "SELECT ".$field." FROM siswa WHERE id_siswa='".$id."'";
+    $exec = mysql_query($qry);
+    $data = mysql_fetch_array($exec);
+    $text = $data[$item];
+    return $text;
 }
